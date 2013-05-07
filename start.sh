58ce95c10454c924ff85edae2732db3301a5c1a5
grayson <cgg5207@sina.com>
2013-05-07 10:04:42 +0800

    start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+
 bundle install
+rake assets:clean
+rake assets:precompile
+sudo kill `sudo netstat -alntp | grep LISTEN | grep 9002 | awk '{print $7}' | cut -f 1 -d '/'`
 nohup bundle exec thin start -p 9002 -e production > log/server.log 2>&1 &
 
